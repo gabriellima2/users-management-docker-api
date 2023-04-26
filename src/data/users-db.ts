74e4b7288481bca7d6f8335fca164f4fb52f8ac5
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-26 13:50:00 -0300

    feat: add users mocked

---
+++ src/data/users-db.ts
@@ -0,0 +1,59 @@
+import type { UserEntity } from "../entities/user-entity";
+
+const users: UserEntity[] = [
+  {
+    id: "0",
+    name: "user_0",
+    email: "any_email_0@gmail.com"
+  },
+  {
+    id: "1",
+    name: "user_1",
+    email: "any_email_1@gmail.com"
+  },
+  {
+    id: "2",
+    name: "user_2",
+    email: "any_email_2@gmail.com"
+  },
+  {
+    id: "3",
+    name: "user_3",
+    email: "any_email_3@gmail.com"
+  },
+  {
+    id: "4",
+    name: "user_4",
+    email: "any_email_4@gmail.com"
+  },
+  {
+    id: "5",
+    name: "user_5",
+    email: "any_email_5@gmail.com"
+  },
+  {
+    id: "6",
+    name: "user_6",
+    email: "any_email_6@gmail.com"
+  },
+  {
+    id: "7",
+    name: "user_7",
+    email: "any_email_7@gmail.com"
+  },
+  {
+    id: "8",
+    name: "user_8",
+    email: "any_email_8@gmail.com"
+  },
+  {
+    id: "9",
+    name: "user_9",
+    email: "any_email_9@gmail.com"
+  },
+  {
+    id: "10",
+    name: "user_10",
+    email: "any_email_10@gmail.com"
+  },
+];
